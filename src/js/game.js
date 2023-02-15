9106494883ea508631d55d728084d3d401691785
Brishna Bakshev <b.bakshev@gmail.com>
2023-02-15 11:32:10 -0800

    fixed images

--- src/js/game.js
+++ src/js/game.js
@@ -1,7 +1,7 @@
 export default function Game() {
   this.totalDays = 0;
   this.daysLeft = 100;
-  this.imgArray =  ["url(../assets/images/travel3.png)", "url(../assets/images/travel2.png)", "url(../assets/images/travel1.png)",];
+  this.imgArray =  ["url(src/assets/images/car1.png)", "url(src/assets/images/car2.png)", "url(src/assets/images/car3.png)"];
   this.imgArrayIndex = 0;
 
   this.addDay = function() {
